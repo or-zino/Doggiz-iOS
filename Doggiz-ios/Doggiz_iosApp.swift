202a91f1929f2e5588afb42db386848ed291f31c
orzino <orzino18@gmail.com>
2022-06-13 18:27:23 +0300

    Initial Commit

---
+++ Doggiz-ios/Doggiz_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doggiz_iosApp.swift
+//  Doggiz-ios
+//
+//  Created by Or Zino on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Doggiz_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
